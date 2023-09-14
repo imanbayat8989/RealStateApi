4daba4cec217dbc9e2cbd1c3868236a85ab734f7
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-14 21:04:22 +0330

    AddxmlSerializer

--- RealStateApi/Models/Category.cs
+++ RealStateApi/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace RealStateApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealStateApi.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Category Name cant be null or empty")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Image Url Name cant be null or empty")]
         public string ImageUrl { get; set; }
         public string Description { get; set; }
     }
