582b619c3f237bba31ea88e07e7f8212ec8b518e
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-15 12:49:18 +0330

    09/15/2023 Adding UserConroller

---
+++ RealStateApi/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealStateApi.Data;
+using RealStateApi.Models;
+
+namespace RealStateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        ApiDbContext _dbContext = new ApiDbContext();
+        [HttpPost("[action]")]
+        public IActionResult Register([FromBody] User user)
+        {
+           var userExists = _dbContext.Users.FirstOrDefault(u=> u.Email == user.Email);
+            if (userExists != null)
+            {
+                return BadRequest("User With Same email already exists");
+            }
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
