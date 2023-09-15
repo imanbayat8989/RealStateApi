fd6f807e4aad36ecea62ff9d63068f2210ee0dea
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-15 17:28:27 +0330

    09/15/2023 Secure the Apis

---
+++ RealStateApi/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RealStateApi.Data;
+
+namespace RealStateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ApiDbContext _dbContext = new ApiDbContext();
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok(_dbContext.Categories);
+        }
+    }
+}
