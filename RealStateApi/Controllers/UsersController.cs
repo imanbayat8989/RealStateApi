7c07e6a20da8034338ce7cf89cc21a2a2527fe11
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-09-15 13:53:37 +0330

    09/15/2023 Login Method

--- RealStateApi/Controllers/UsersController.cs
+++ RealStateApi/Controllers/UsersController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RealStateApi.Data;
 using RealStateApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace RealStateApi.Controllers
 {
@@ -10,6 +14,12 @@ namespace RealStateApi.Controllers
     public class UsersController : ControllerBase
     {
         ApiDbContext _dbContext = new ApiDbContext();
+        private IConfiguration _config;
+        public UsersController(IConfiguration config)
+        {
+              _config = config;
+        }
+
         [HttpPost("[action]")]
         public IActionResult Register([FromBody] User user)
         {
@@ -21,6 +31,30 @@ namespace RealStateApi.Controllers
             _dbContext.Users.Add(user);
             _dbContext.SaveChanges();
             return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Login([FromBody] User user)
+        {
+           var currentUser = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email && u.Password==user.Password);
+            if (currentUser == null)
+            {
+                return NotFound();
+            }
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                claims: claims,
+                expires:DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials);
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(jwtToken);
         }
     }
 }
